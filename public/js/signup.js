9ae407dae06ca9f0e1db00d246d4f3890fcf483a
Bryan A Quero <bryanq2008@live.com>
2023-01-19 17:01:47 -0500

    update # 3 Handlebars updated

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,34 @@
-const signupbtn = doc
+const signupbtn = document.querySelectorAll('#signupbtn');
+signupbtn.addEventListener('click',async (e) => {
+
+    e.preventDefault();
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+    // const password2 = document.querySelector('#password2').value.trim();
+  
+
+
+    const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({ 
+            name: name,
+            email: email,
+            password: password,
+            // password2: password2
+
+        }),
+
+        headers: { 'Content-Type': 'application/json' },
+
+});
+
+if (response.ok) {
+    document.location.replace('/dashboard');
+} else {
+    document.querySelector('signup-error').textContent = 'Failed to sign up';
+}
+
+});
+
+
